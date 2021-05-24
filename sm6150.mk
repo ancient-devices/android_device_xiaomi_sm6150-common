a02c4f7a53989740b6c1568b395ff4f599dc0c55
Genkzsz11 <Genkzsz11@gmail.com>
2021-05-24 13:30:52 +0700

    sm6150-common: Update thermal & Perf blobs from Q
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -262,7 +262,7 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 
 # Perf
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2.vendor
+    vendor.qti.hardware.perf@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/commonresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonresourceconfigs.xml \
